39b5e9ce3576b7789f04e87e9e38e093e3a4b4ad
emerk777 <emerk777@yahoo.com>
2021-07-30 19:32:36 -0600

    Restful Demo

---
+++ src/test/java/com/example/Spring5RestfulDemo/Spring5RestfulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring5RestfulDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RestfulDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
